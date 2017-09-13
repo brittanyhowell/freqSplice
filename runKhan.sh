6a4e45de4f8a48dd03084c5298469640300505fc
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2017-09-13 17:31:24 +0930

    actually runs Khan

--- runKhan.sh
+++ runKhan.sh
@@ -1,24 +1,48 @@
 #!/bin/bash
 
 scriptDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/freqSplice/scripts
-wkDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/freqSplice/gapTables/human1045
+wkDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/freqSplice/gapTables/human1045Aggregate
+outDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/freqSplice/gapTablePlotsTables/human1045Aggregate
+
+if [ -d $outDIR ]; then
+    rm -r $outDIR
+    echo "Folder $outDIR exists... replacing"
+    mkdir $outDIR
+else
+    mkdir $outDIR
+fi
 
 cd ${scriptDIR}
-
 cp khan.R ${wkDIR}
-
-
 cd ${wkDIR}
 
-sample=$(ls *report.txt)
+echo "removing half baked goods"
+rm *_plot_splits.pdf 
+rm *_table_splits.txt 
+rm *_plot_L1s.pdf 
+rm *_plot_coverage.pdf
+
+
+sample=$(ls *intervals.txt)
 
 for gap in ${sample}; do
 
-	sampleName="${gap%_report.txt}" 
+	sampleName="${gap%_intervals.txt}" 
 	noGAP="$(echo $sampleName | sed 's/gapsIn//g')" # sample name generated
 
 	intTable=${sampleName}_intervals.txt
+	readSumTable=${sampleName}_readSummary.txt
+	splitTable=${wkDIR}/${sampleName}_splitReads.txt
 
-	echo ${intTable}
-	wc -l ${intTable}
+echo ${sampleName}
+
+	Rscript khan.R ${wkDIR} ${intTable} ${sampleName}_plot_coverage.pdf ${sampleName} ${readSumTable} ${sampleName}_plot_L1s.pdf ${splitTable} ${sampleName}_plot_splits.pdf ${sampleName}_table_splits.txt
 done
+
+mv *_plot_splits.pdf ${outDIR}
+mv *_table_splits.txt ${outDIR}
+mv *_plot_L1s.pdf ${outDIR}
+mv *_plot_coverage.pdf ${outDIR}
+
+
+echo "complete"
