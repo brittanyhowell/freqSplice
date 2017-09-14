844751088a1f6fb462ac24dacb5e3b844280f055
brittanyhowell <brittany.howell@student.adelaide.edu.au>
2017-09-14 14:55:34 +0930

    Used while loop. Don't use while loops.

--- findL1Reads.sh
+++ findL1Reads.sh
@@ -2,17 +2,31 @@
 
 # This script uses the readSummary from kamala.go. It uses the  coords from readSummary to extract just the matching records from the total reads document. 
 
-reads=gapsIn24h_1mg_R2_reads.txt 
-keys=onlySpliceL1.txt
+wkDIR=/Users/brittanyhowell/Documents/University/Honours_2016/Project/freqSplice/gapTables/human1045
 
-list=$( awk '{print $2}' ${keys})
+readSum=gapsIn0h_Ctrl_R2_readSummary.txt
+keys=gapsIn0h_Ctrl_R2_70L1KEY.txt
+reads=gapsIn0h_Ctrl_R2_reads.txt
 
-touch onlyReadsL1.txt
+# Make keys list from highly spliced L1s
+# awk '{if ($7 > 70) print}' ${readSum} > ${keys}
 
-for num in ${list} ; do
-	# grep $num ${reads} >> onlyReadsL1.txt
-	echo $num
-done
+cd ${wkDIR}
+
+while read  chr  startL1  endL1 
+do
+     echo "${chr}, ${startL1}, ${endL1}!"
+    cat ${reads} | awk '{if ($2 == $chr ) print}'
+done < ${keys}
+
+
+echo "we done, tell Jack you love him"
+# touch onlyReadsL1.txt
+
+# for num in ${list} ; do
+# 	# grep $num ${reads} >> onlyReadsL1.txt
+# 	echo $num
+# done
 
 
 
